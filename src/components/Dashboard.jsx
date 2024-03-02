4962f8e9270a8ae8ed6a0e8b85e4f6676b98b022
Sushant Sharma <sushant.ku.sh.70@gmail.com>
2024-03-02 17:03:30 +0545

    :feat: chnages

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,26 +4,34 @@ import ChartB from "./ChartB";
 
 function Dashboard() {
   return (
-    <div className="px-10 py-14 flex flex-col">
+    <div className="px-10 py-14 w-full flex flex-col">
       <h2 className=" mb-5 bg-[#FF6347] px-5 py-3  w-full text-xl font-bold">
         Dashboard
       </h2>
       <div className=" flex space-x-8 mb-10">
-        <div className=" h-[150px] w-[480px] border border-gray-500 rounded flex flex-col  items-start justify-center p-4 mt-5">
-          <span>Sushant Sharma</span>
-          <span>Your Balance is RS 1000000</span>
+        <div className=" h-[150px] w-[280px] border border-gray-500 rounded flex flex-col shadow-md  items-start justify-center p-4 mt-5">
+          <span className=" text-xl font-semibold">Sushant Sharma</span>
+          <span className=" font-light">Your Balance is RS 1000000</span>
         </div>
-        <div className=" h-[150px] w-[480px] border border-gray-500 rounded flex flex-col  items-start justify-center p-4 mt-5">
-          <span>Sushant Sharma</span>
-          <span>Your Balance is RS 1000000</span>
+        <div className=" h-[150px] w-[280px] border border-gray-500 rounded flex flex-col shadow-md   items-start justify-center p-4 mt-5">
+          <span className=" text-xl font-semibold">Hari Prasad </span>
+          <span className=" font-light">Your Balance is RS 450000</span>
         </div>
-        <div className=" h-[150px] w-[480px] border border-gray-500 rounded flex flex-col  items-start justify-center p-4 mt-5">
-          <span>Sushant Sharma</span>
-          <span>Your Balance is RS 1000000</span>
+        <div className=" h-[150px] w-[280px] border border-gray-500 rounded flex flex-col shadow-md   items-start justify-center p-4 mt-5">
+          <span className=" text-xl font-semibold">Ram Baran Yadav</span>
+          <span className=" font-light">Your Balance is RS 390000</span>
+        </div>
+        <div className=" h-[150px] w-[280px] border border-gray-500 rounded flex flex-col shadow-md  items-start justify-center p-4 mt-5">
+          <span className=" text-xl font-semibold">Sneha Thakur</span>
+          <span className=" font-light">Your Balance is RS 700000</span>
+        </div>
+        <div className=" h-[150px] w-[280px] border border-gray-500 rounded flex flex-col shadow-md  items-start justify-center p-4 mt-5">
+          <span className=" text-xl font-semibold">Sanu Acharya</span>
+          <span className=" font-light">Your Balance is RS 390000</span>
         </div>
       </div>
       <div className=" flex mb-10">
-        <div className=" flex flex-col space-x-8 w-full  h-[40vh]">
+        <div className=" flex w-full flex-col space-x-8 w-full  h-[40vh]">
           <h2 className=" mb-5 bg-[#FF6347] px-5 py-3  text-xl font-bold">
             Expenses Chart
           </h2>
